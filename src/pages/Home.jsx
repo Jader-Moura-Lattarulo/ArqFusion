6da0a9a533c3101c96f08af543f775f8b6e5bd7a
Jader Moura <jader.dev.moura@gmail.com>
2025-03-24 14:05:19 -0300

    Feat: Header Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
+//COMPONENT
+import Header from "../components/Header/Header"
+
 function Home() {
     return (
         <>
+        <Header/>
             <h1>HOME</h1>
             <p>Subtitle</p>
         </>
