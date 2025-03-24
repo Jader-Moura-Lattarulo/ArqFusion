0eed0b25e8c35d42709c94966ac6cde9ff27c705
Jader Moura <jader.dev.moura@gmail.com>
2025-03-24 12:23:39 -0300

    Feat: create pages

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+        <>
+            <h1>CONTACT</h1>
+            <p>Subtitle Contact</p>
+        </>
+    )
+}
+
+export default Contact
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>HOME</h1>
+            <p>Subtitle</p>
+        </>
+    )
+}
+
+export default Home
