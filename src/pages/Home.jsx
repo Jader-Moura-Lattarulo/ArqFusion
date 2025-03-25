96c3fb6e8def33b0665e3a6d4cdcfa690fcc2512
Jader Moura <jader.dev.moura@gmail.com>
2025-03-25 14:25:21 -0300

    Feat: create hero component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,15 @@
 //COMPONENT
-import Banner from "../components/Banner/Banner"
-import Button from "../components/Button/Button"
 import Header from "../components/Header/Header"
+import Hero from "../components/Hero/Hero"
 import Footer from "../components/Footer/Footer"
 
 function Home() {
     return (
         <>
         <Header/>
-            <Banner title="Home" image="about.jpg"></Banner>
-            <h1>HOME</h1>
-            <p>Subtitle</p>
-            <Button buttonStyle="primary" arrow>Primary!</Button>
-            <Button buttonStyle="secondary">Secondary!</Button>
-            <Button buttonStyle="outline">Outline!</Button>
-            <Button buttonStyle="disabled" disabled>Disabled!</Button>
+        <div className="container">
+            <Hero/>
+        </div>
         <Footer/>
         </>
     )
