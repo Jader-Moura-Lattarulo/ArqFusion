5155dba35a53631256720c02461e9c3086708272
Jader Moura <jader.dev.moura@gmail.com>
2025-03-25 17:04:24 -0300

    Feat: aboutText component

---
+++ src/components/AboutText/AboutText.jsx
@@ -0,0 +1,20 @@
+import './AboutText.css'
+
+function AboutText() {
+    return (
+        <div className='container'>
+            <div className='text-section d-flex'>
+                <div className='text-section-text d-flex'>
+                    <h2>We set the trends of modern living Services.</h2>
+                </div>
+                <div className='text-section-text d-flex fd-column'>
+                    <p className='primary-color'>Primeira Linha</p>
+                    <p>Segunda Linha</p>
+                    <p>Terceira Linha</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutText
